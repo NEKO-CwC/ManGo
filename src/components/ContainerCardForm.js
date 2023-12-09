e42b79b01948cad3f27cac614d3f7a7845a57c6c
Ray <675983230@qq.com>
2023-12-09 19:21:12 +0800

    test

---
+++ src/components/ContainerCardForm.js
@@ -0,0 +1,51 @@
+import "./ContainerCardForm.css";
+
+const ContainerCardForm = () => {
+  return (
+    <div className="div30">
+      <div className="rectangle-parent13">
+        <div className="group-child991" />
+        <div className="group-child992" />
+        <div className="group-child993" />
+        <div className="group-child994" />
+        <div className="group-child995" />
+        <div className="group-child996" />
+        <div className="group-child997" />
+        <div className="group-child998" />
+        <div className="group-child999" />
+        <div className="group-child1000" />
+        <div className="group-child1001" />
+        <div className="group-child1002" />
+        <div className="group-child1003" />
+        <div className="group-child1004" />
+        <div className="group-child1005" />
+      </div>
+      <div className="child663" />
+      <div className="child664" />
+      <div className="child665" />
+      <div className="child666" />
+      <div className="child667" />
+      <div className="child668" />
+      <div className="child669" />
+      <div className="child670" />
+      <div className="child671" />
+      <div className="child672" />
+      <div className="child673" />
+      <div className="child674" />
+      <div className="child675" />
+      <div className="child676" />
+      <div className="child677" />
+      <div className="child678" />
+      <div className="child679" />
+      <div className="child680" />
+      <div className="child681" />
+      <div className="child682" />
+      <div className="child683" />
+      <div className="child684" />
+      <div className="child685" />
+      <div className="child686" />
+    </div>
+  );
+};
+
+export default ContainerCardForm;
